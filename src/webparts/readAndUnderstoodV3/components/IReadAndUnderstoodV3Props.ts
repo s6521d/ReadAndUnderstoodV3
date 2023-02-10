a98afa8b5909b055d9be0245462ae7451b49ccdd
Steve Dawkins <steve@spadditions.com>
2023-02-10 16:58:23 +0000

    first commit

---
+++ src/webparts/readAndUnderstoodV3/components/IReadAndUnderstoodV3Props.ts
@@ -0,0 +1,7 @@
+export interface IReadAndUnderstoodV3Props {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/readAndUnderstoodV3/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IReadAndUnderstoodV3WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ReadAndUnderstoodV3WebPartStrings' {
+  const strings: IReadAndUnderstoodV3WebPartStrings;
+  export = strings;
+}
